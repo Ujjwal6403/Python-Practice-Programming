9a7166d158b13d11f2352a0b85d304320ac654bc
ujwal <ujjwalnarkhede2003@gmail.com>
2025-08-14 10:54:14 +0530

    Added RevString.py

---
+++ RevString.py
@@ -0,0 +1,11 @@
+def Fun(name):
+   Namenew=[]
+   name=list(name)
+   count=name.count
+   if name!="":
+      for a in name:
+         Namenew.insert(a)
+      print(Namenew)
+         
+
+Fun("ujjwal")
